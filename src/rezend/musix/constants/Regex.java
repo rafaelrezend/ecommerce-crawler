32588143cb9d47425fd865d7041b2beb02f6bd9e
Rafael Rezende <rafaelrezend@gmail.com>
2013-08-27 02:57:47 +0200

    ContentParser and corresponding Regex added.

---
+++ src/rezend/musix/constants/Regex.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package rezend.musix.constants;
+
+/**
+ * This class holds the regular expressions used to identify values and links
+ * within a URL content.
+ * 
+ * @author Rafael Rezende
+ * 
+ */
+public class Regex {
+	
+	public final static String M_PRODUKTIV_PRODUCT = "<h2 style=\"text-align:left;\">(.*?)</h2>";
+	public final static String M_PRODUKTIV_ID = "<div .*?Bestellnummer: (.*?)</div>";
+	public final static String M_PRODUKTIV_PRICE_LINK = "<b>Unser Preis:</b></span> <img src=\"(.*?)\".*?/>";
+
+}
---
+++ src/rezend/musix/tool/ContentParser.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package rezend.musix.tool;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import rezend.musix.ErrorCodes;
+
+/**
+ * This class is used to parse the content of a String using regular expression.
+ * It was developed to extract specific values and links from websites. This
+ * task could be performed by HTML parser. However, since the tags of the
+ * desired values are not well defined, a regex parser tends to be more
+ * flexible.
+ * 
+ * @author Rafael Rezende
+ * 
+ */
+public final class ContentParser {
+
+	/**
+	 * This method parses the urlContent using a regular expression looking for
+	 * a unique value. Multiple values matching the same regex is an illegal
+	 * condition, so a warning is raised. Regex should be provided as the
+	 * example: "<h2 style=\"text-align:left;\">(.*?)</h2>"
+	 * 
+	 * @param urlContent
+	 *            Holds the content of a web page.
+	 * @param regex
+	 *            Regular expression used to identify values and links
+	 * @return The desired value or link
+	 */
+	public static String parseContent(String urlContent, String regex) {
+
+		// this pattern is specific for the given website
+		Pattern pattProduct = Pattern.compile(regex, Pattern.DOTALL);
+
+		// create a matcher from the given pattern for the URL content
+		Matcher matcher = pattProduct.matcher(urlContent);
+
+		// find the first pattern match
+		matcher.find();
+
+		// extract the value in between (.*?)
+		String result = matcher.group(1);
+
+		// there should be only one valid match in a given page. If more than
+		// one match is found, then a warning is raised
+		if (matcher.find())
+			System.err.println(ErrorCodes.MULTIPLE_MATCH);
+
+		return result;
+	}
+}
