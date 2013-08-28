c4fd53d14654d729cb2ea4c3f95b61c1663b3887
Rafael Rezende <rafaelrezend@gmail.com>
2013-08-28 03:36:28 +0200

    Main file organized with producer and consumer.

--- src/rezend/musix/Main.java
+++ src/rezend/musix/Main.java
@@ -2,10 +2,24 @@ package rezend.musix;
 
 import rezend.musix.core.ProductDAO;
 import rezend.musix.core.Product;
+import rezend.musix.core.LinksList;
 import rezend.musix.core.ProductParser;
+import rezend.musix.crawler.CrawlerControl;
 
 /**
- * Preferably open-source libraries
+ * This application contains a web crawler that fetches every link from a
+ * reference website. Links are processed in parallel so that those that
+ * corresponds to product page are extracted and have their data stored to a
+ * MySQL database. Products' price are exhibit as graphics. Therefore, an OCR
+ * process is applied to extract the values.
+ * 
+ * Since it depends on OCR to obtain the relevant data, the success rate depends
+ * on the quality of the graphics representing the values. This way, the
+ * efficiency of this application cannot be fully guaranteed.
+ * 
+ * Crawler+producer and consumer are executed in different threads. They can run
+ * indefinitely. However, a limit of products is set so that the application can
+ * proceed retrieving and showing the stored data.
  * 
  * @author Rafael Rezende
  * 
@@ -17,10 +31,31 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		Product prod = ProductParser.parseProductFromURL("http://www.musik-produktiv.ch/boss-loop-station-rc-300.aspx");
-		
+		// creation of the unique instance containing the list of links
+		LinksList pList = LinksList.getInstance();
+		// connect to database and create table if not exists
 		ProductDAO.connect();
-		ProductDAO.addProduct(prod);
+
+		// starts the web crawler in parallel
+		CrawlerControl.startCrawler();
+		
+		// routine to consume the links from the list of links
+		for (int productCounter = 0; productCounter < 100;) {
+			String urlStr = pList.get();
+			Product prod = ProductParser.parseProductFromURL(urlStr);
+			if (prod == null)
+				System.out.println("No product found in the page: " + urlStr);
+			else {
+				ProductDAO.addProduct(prod);
+				productCounter++;
+			}
+		}
+
+		/* example of how a product is processed and stored */
+		// prod = ProductParser
+		// .parseProductFromURL("http://www.musik-produktiv.ch/rohema-44-cm.aspx");
+		// ProductDAO.addProduct(prod);
+		
 		ProductDAO.printProducts();
 	}
 }
